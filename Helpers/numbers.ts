a13cb05ccc42c2fe6fb2efd261750747fd4c1dae
codemodify <codemodify@member.fsf.org>
2020-01-27 01:51:17 -0800

    update the rng

--- Helpers/numbers.ts
+++ Helpers/numbers.ts
@@ -1,17 +1,26 @@
 namespace SystemKit.Helpers {
 
 	export function RandomInt(min: number, max: number): number {
-		var byteArray = new Uint8Array(1);
-		crypto.getRandomValues(byteArray);
+		// var byteArray = new Uint8Array(1);
+		// crypto.getRandomValues(byteArray);
 
-		var range = max - min + 1;
-		var maxRange = 256;
+		// var range = max - min + 1;
+		// var maxRange = 256;
 
-		if (byteArray[0] >= Math.floor(maxRange / range) * range) {
-			return RandomInt(min, max);
-		}
+		// if (byteArray[0] >= Math.floor(maxRange / range) * range) {
+		// 	return RandomInt(min, max);
+		// }
 
-		return min + (byteArray[0] % range);
+		// return min + (byteArray[0] % range);
+
+		const randomBuffer = new Uint32Array(1);
+		crypto.getRandomValues(randomBuffer);
+
+		let randomNumber = randomBuffer[0] / (0xffffffff + 1);
+
+		min = Math.ceil(min);
+		max = Math.floor(max);
+		return Math.floor(randomNumber * (max - min + 1)) + min;
 	}
 
 	export function RandomFloat(min: number, max: number): number {
