4d175f34f378751cb1b4c75aef077f36b8231e4d
codemodify <codemodify@member.fsf.org>
2020-01-28 16:45:17 -0800

    Add object serialization PascalCase and camelCase

--- Helpers/objects.ts
+++ Helpers/objects.ts
@@ -29,4 +29,62 @@ namespace SystemKit.Helpers {
 	export function CloneObject(object: any): any {
 		return JSON.parse(JSON.stringify(object));
 	}
+
+	export function CloneObjectLowerMemberFirstChar(o) {
+		var newO, origKey, newKey, value;
+
+		// array
+		if (o instanceof Array) {
+			return o.map(function (value) {
+				if (typeof value === "object") {
+					value = CloneObjectLowerMemberFirstChar(value)
+				}
+				return value
+			})
+		}
+
+		// object
+		newO = {}
+		for (origKey in o) {
+			if (o.hasOwnProperty(origKey)) {
+				newKey = (origKey.charAt(0).toLowerCase() + origKey.slice(1) || origKey).toString()
+				value = o[origKey]
+				if (value instanceof Array || (value !== null && value.constructor === Object)) {
+					value = CloneObjectLowerMemberFirstChar(value)
+				}
+				newO[newKey] = value
+			}
+		}
+
+		return newO
+	}
+
+	export function CloneObjectUpperMemberFirstChar(o) {
+		var newO, origKey, newKey, value;
+
+		// array
+		if (o instanceof Array) {
+			return o.map(function (value) {
+				if (typeof value === "object") {
+					value = CloneObjectUpperMemberFirstChar(value)
+				}
+				return value
+			})
+		}
+
+		// object
+		newO = {}
+		for (origKey in o) {
+			if (o.hasOwnProperty(origKey)) {
+				newKey = (origKey.charAt(0).toUpperCase() + origKey.slice(1) || origKey).toString()
+				value = o[origKey]
+				if (value instanceof Array || (value !== null && value.constructor === Object)) {
+					value = CloneObjectUpperMemberFirstChar(value)
+				}
+				newO[newKey] = value
+			}
+		}
+
+		return newO
+	}
 }
